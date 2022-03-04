32d421447311b774b8c5078c1434cd19d865289a
Felipe Lalanne <felipe@balena.io>
2022-03-04 15:38:21 -0300

    Create map() state transformer

--- lib/goal/state.ts
+++ lib/goal/state.ts
@@ -4,3 +4,15 @@
 export type State<TContext = any, TState = any> = (
 	c: TContext,
 ) => Promise<TState>;
+
+/**
+ * Transform a state that receives a context A into a state that receives
+ * a context B by applying a transformation function
+ */
+export const map =
+	<TOtherContext = any, TContext = any, TState = any>(
+		s: State<TContext, TState>,
+		f: (c: TOtherContext) => TContext,
+	): State<TOtherContext, TState> =>
+	(c: TOtherContext) =>
+		s(f(c));
