0f3fb4741e4fb580867e6b2979873c07ae8f2a52
Felipe Lalanne <felipe@balena.io>
2022-04-19 12:16:51 -0400

    Setup library exports

--- lib/goal/index.ts
+++ lib/goal/index.ts
@@ -1,1 +1,4 @@
 export * from './goal';
+export { State } from './state';
+export { Test } from './test';
+export { Action } from './action';
--- lib/index.ts
+++ lib/index.ts
@@ -1,11 +1,1 @@
-export interface ImplementMe {
-	myFunc(): Promise<string>;
-}
-
-const moo = 1;
-
-export class ImplementClass implements ImplementMe {
-	public async myFunc() {
-		return `I need implementing! ${moo}`;
-	}
-}
+export * from './goal';
